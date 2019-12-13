279de69b4a8f8c317c596f4d551c8c1f50f9833b
frivard <frivard@coveo.com>
2019-12-13 10:19:59 -0500

    Changed the grant script to specify only the environment

--- GrantCloudOps.psm1
+++ GrantCloudOps.psm1
@@ -1,10 +1,23 @@
 function Grant-CloudOps
 {
 param (
-    [string]$clientSecret,
-    [string]$username = "CloudOpsAutomation",
-    [string]$authServerUri = "https://platformdev.cloud.coveo.com/oauth/token?grant_type=client_credentials"
+    [string]$env = "dev",
+    [string]$username = "CloudOpsAutomation"
  )
+$ssmNames = @{
+    dev = "/dev/ops/OAuth/CloudOpsAutomation/clientSecret";
+    qa = "/qa/ops/OAuth/CloudOpsAutomation/clientSecret"
+}
+$authServers = @{
+    dev = "https://platformdev.cloud.coveo.com/oauth/token?grant_type=client_credentials";
+    qa = "https://platformqa.cloud.coveo.com/oauth/token?grant_type=client_credentials"
+}
+Write-Host "environment is $env"
+$ssmParam = $ssmNames[$env]
+$authServerUri = $authServers[$env]
+Write-Host "ssm is $ssmParam and server is $authServerUri"
+$clientSecret = ((aws ssm get-parameter --region us-east-1 --name $ssmParam --with-decryption) | ConvertFrom-Json).Parameter.Value
+Write-Host "got secret $clientSecret"
 $pair = "$($username):$($clientSecret)"
 $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
 $basicAuthValue = "Basic $encodedCreds"
