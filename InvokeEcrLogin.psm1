78a3221890ccf0586ab9a0fb1f89381d6a2dd106
frivard <frivard@coveo.com>
2020-09-22 10:16:34 -0400

    Update ECR login with password stdin

--- InvokeEcrLogin.psm1
+++ InvokeEcrLogin.psm1
@@ -1,4 +1,6 @@
 function Invoke-EcrLogin
 {
-  Invoke-Expression (aws ecr get-login --no-include-email --region us-east-1)
+    $region = "us-east-1"
+    $account = "064790157154"
+    aws ecr get-login-password "--region" "$region" | docker "login" "--username" "AWS" "--password-stdin" "$account.dkr.ecr.$region.amazonaws.com"
 }
