286d35a8d17bcf2730e36fc83409039e901c7d27
frivard <frivard@coveo.com>
2020-10-09 09:46:52 -0400

    Add possibility to enter VS Dev in Preview

--- EnterVsDev.psm1
+++ EnterVsDev.psm1
@@ -1,7 +1,16 @@
 function Enter-VsDev
 {
+  param (
+    [switch]$preview = $false
+  )
+
+  if ($preview){
+    Import-Module 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+    Enter-VsDevShell -SetDefaultWindowTitle -InstallPath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview' -StartInPath $pwd.Path -DevCmdArguments '-arch=x64 -no_logo'
+  } else {
     Import-Module 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
     Enter-VsDevShell -SetDefaultWindowTitle -InstallPath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise' -StartInPath $pwd.Path -DevCmdArguments '-arch=x64 -no_logo'
+  }
 }
 
 function covb
